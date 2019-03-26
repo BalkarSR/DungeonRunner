1b80de667aff27b0f4e17d2d141b54117396d1dd
Balkar Rana <balkar_rana@hotmail.com>
2019-03-26 00:38:54 -0400

    moss getting killed and animation playing accordingly

--- Assets/Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Assets/Scripts/Enemy/Enemy.cs
@@ -7,6 +7,9 @@ public abstract class Enemy : MonoBehaviour
     //Create a vector3 object to get current Target positions
     public Vector3 targetLoc;
 
+    //to see if enemy is hit
+    public bool isHit = false;
+
     //ref to animator of the moss sprite
     public Animator animator;
 
@@ -19,6 +22,9 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField]
     public Transform locA, locB;
 
+    //variable to store the player
+    public Player player;
+
     //to start and initalize the objects
     public void Start()
     {
@@ -28,51 +34,68 @@ public abstract class Enemy : MonoBehaviour
     //to ensure every enemy we create in the future can include this method
     public virtual void Update()
     {
+        //if idle animation is playing and inCombat is false then do nothing, otherwise start moving
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") && animator.GetBool("InCombat") == false)
+        {
+            return;
+        }
+
         //call the movement for all enemies
         Movement();
     }
 
     public virtual void Movement()
     {
-        //checks if idle animation is not playing, then start moving
-        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        //flip the moss accordingly to which way its going
+        if (targetLoc == locA.position)
         {
+            //flip the moss on the x axis
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            //flip the moss on the x axis
+            spriteRenderer.flipX = false;
+        }
 
-            //flip the moss accordingly to which way its going
-            if (targetLoc == locA.position)
-            {
-                //flip the moss on the x axis
-                spriteRenderer.flipX = true;
-            }
-            else
-            {
-                //flip the moss on the x axis
-                spriteRenderer.flipX = false;
-            }
+        //if the current position is at point A
+        if (transform.position == locA.position)
+        {
+            //set the target location to point B
+            targetLoc = locB.position;
 
-            //if the current position is at point A
-            if (transform.position == locA.position)
-            {
-                //set the target location to point B
-                targetLoc = locB.position;
+            //call the idle animation
+            animator.SetTrigger("Idle");
+        }
 
-                //call the idle animation
-                animator.SetTrigger("Idle");
-            }
+        //if the current position is at point B
+        else if (transform.position == locB.position)
+        {
+            //set the target location to point A
+            targetLoc = locA.position;
 
-            //if the current position is at point B
-            else if (transform.position == locB.position)
-            {
-                //set the target location to point A
-                targetLoc = locA.position;
+            //call the idle animation
+            animator.SetTrigger("Idle");
+        }
 
-                //call the idle animation
-                animator.SetTrigger("Idle");
-            }
-
+        //if isHit is not true ( meaning enemy is not hit) then keep moving    
+        if (!isHit)
+        {
             //move the character to the targetLoc
             transform.position = Vector3.MoveTowards(transform.position, targetLoc, speed * Time.deltaTime);
         }
+
+        //check for distance between player and enemy
+        //store distance
+        float distance = Vector3.Distance(transform.localPosition, player.transform.localPosition);
+        //if greater than 2 units, isHit = false and inCombt = false
+        if (distance > 2.0f)
+        {
+            isHit = false;
+            animator.SetBool("InCombat", false);
+        }
+
+        
 
     }
 
@@ -80,5 +103,6 @@ public abstract class Enemy : MonoBehaviour
     {
         animator = GetComponentInChildren<Animator>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 }
--- Assets/Assets/Scripts/Enemy/Moss_Giant.cs
+++ Assets/Assets/Scripts/Enemy/Moss_Giant.cs
@@ -2,11 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moss_Giant : Enemy
+public class Moss_Giant : Enemy, IDamageable
 {
+
+    public int hp { get; set; }
+
     //to initalize custom stuff to the moss giant
     public override void Init()
     {
         base.Init();
+
+        //assign total hp to moss
+        base.hp = 5;
+
+        //set the current hp to whatever the base class is
+        this.hp = base.hp;
+
+
     }
+
+    public void Damage()
+    {
+        //subtract the hp by 1 when the dmg method is called
+        hp--;
+
+        //play the hit animation
+        animator.SetTrigger("Hit");
+
+        //enemy is now hit
+        isHit = true;
+
+        //in combat animator is on
+        animator.SetBool("InCombat", true);
+
+        //if hp is less than 1 it means it has 0 hp
+        if(hp < 1)
+        {
+            //destroy the enemy
+            Destroy(this.gameObject);
+        }
+    }
+
 }
